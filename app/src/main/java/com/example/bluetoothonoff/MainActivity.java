8e228f33a4418ebc60c9c54f1007f97032f0f5f1
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-06 16:44:43 +0630

    build project and add bluetooth on off

---
+++ app/src/main/java/com/example/bluetoothonoff/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.bluetoothonoff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    BluetoothAdapter bAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void onBluetoothSetting(View view){
+        if(bAdapter == null){
+            Toast.makeText(this,"Bluetooth is not supported",Toast.LENGTH_LONG).show();
+        }else{
+            if(!bAdapter.isEnabled()){
+                startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
+                Toast.makeText(this,"Bluetooth is turned on",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void offBluetoothSetting(View view){
+        bAdapter.disable();
+        Toast.makeText(this,"Bluetooth is turned off",Toast.LENGTH_LONG).show();
+
+    }
+}
